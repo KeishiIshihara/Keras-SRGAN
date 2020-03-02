12fb02158467580befbb4d2f82b2b3ecb0771e6f
KeishiIshihara <ishihara@aisl.cs.tut.ac.jp>
2020-03-02 17:02:54 +0200

    adopted to our project

--- train.py
+++ train.py
@@ -4,7 +4,7 @@
 #author          :Deepak Birla
 #date            :2018/10/30
 #usage           :python train.py --options
-#python_version  :3.5.4 
+#python_version  :3.5.4
 
 from Network import Generator, Discriminator
 import Utils_model, Utils
@@ -14,13 +14,15 @@ from keras.models import Model
 from keras.layers import Input
 from tqdm import tqdm
 import numpy as np
+import os, sys
 import argparse
 
 np.random.seed(10)
 # Better to use downscale factor as 4
 downscale_factor = 4
 # Remember to change image shape if you are having different size of images
-image_shape = (384,384,3)
+# image_shape = (384,384,3)
+image_shape = (256,256,3)
 
 # Combined network
 def get_gan_network(discriminator, shape, generator, optimizer, vgg_loss):
@@ -54,33 +56,41 @@ def train(epochs, batch_size, input_dir, output_dir, model_save_dir, number_of_i
 
     gan = get_gan_network(discriminator, shape, generator, optimizer, loss.vgg_loss)
 
-    loss_file = open(model_save_dir + 'losses.txt' , 'w+')
+    loss_file = open(os.path.join(model_save_dir, 'losses.txt') , 'w+')
     loss_file.close()
+
+    gan_loss_best = np.inf
+    discriminator_loss_best = np.inf
 
     for e in range(1, epochs+1):
         print ('-'*15, 'Epoch %d' % e, '-'*15)
         for _ in tqdm(range(batch_count)):
 
             rand_nums = np.random.randint(0, x_train_hr.shape[0], size=batch_size)
+            # rand_nums = np.random.choice(np.arange(x_train_hr.shape[0]), size=batch_size, replace=False)
 
+            # get batch data
             image_batch_hr = x_train_hr[rand_nums]
             image_batch_lr = x_train_lr[rand_nums]
-            generated_images_sr = generator.predict(image_batch_lr)
+            generated_images_sr = generator.predict(image_batch_lr) # get generator prediction
 
-            real_data_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
-            fake_data_Y = np.random.random_sample(batch_size)*0.2
+            real_data_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2 # biased random noise for real
+            fake_data_Y = np.random.random_sample(batch_size)*0.2 # biased random noise for fake
 
+            # train discriminator
             discriminator.trainable = True
 
             d_loss_real = discriminator.train_on_batch(image_batch_hr, real_data_Y)
             d_loss_fake = discriminator.train_on_batch(generated_images_sr, fake_data_Y)
             discriminator_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
 
+            # get batch data
             rand_nums = np.random.randint(0, x_train_hr.shape[0], size=batch_size)
             image_batch_hr = x_train_hr[rand_nums]
             image_batch_lr = x_train_lr[rand_nums]
 
-            gan_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
+            # train gan (do not train discriminator)
+            gan_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2 # biased noise for y
             discriminator.trainable = False
             gan_loss = gan.train_on_batch(image_batch_lr, [image_batch_hr,gan_Y])
 
@@ -89,28 +99,41 @@ def train(epochs, batch_size, input_dir, output_dir, model_save_dir, number_of_i
         print("gan_loss :", gan_loss)
         gan_loss = str(gan_loss)
 
-        loss_file = open(model_save_dir + 'losses.txt' , 'a')
+        loss_file = open(os.path.join(model_save_dir, 'losses.txt') , 'a')
         loss_file.write('epoch%d : gan_loss = %s ; discriminator_loss = %f\n' %(e, gan_loss, discriminator_loss) )
         loss_file.close()
 
         if e == 1 or e % 5 == 0:
             Utils.plot_generated_images(output_dir, e, generator, x_test_hr, x_test_lr)
-        if e % 500 == 0:
+        if e % 100 == 0:
             generator.save(model_save_dir + 'gen_model%d.h5' % e)
             discriminator.save(model_save_dir + 'dis_model%d.h5' % e)
+        if gan_loss < gan_loss_best:
+            gan_loss_best = gan_loss
+            generator.save(os.path.join(model_save_dir, 'gen_model{}_by_loss_monitor.h5'.format(e)))
+        if discriminator_loss < discriminator_loss_best:
+            discriminator_loss_best = discriminator_loss
+            discriminator.save((model_save_dir, 'dis_model{}_by_loss_monitor.h5'.format(e)))
 
 
 if __name__== "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--input_dir', action='store', dest='input_dir', default='./data/' ,
+    parser.add_argument('-i', '--input_dir', action='store', dest='input_dir',
+                    # default='./data/' ,
+                    default='/media/ssd1/srgan_dataset/images/faces',
+                    # default='/media/ssd1/srgan_dataset/images/landscapes',
                     help='Path for input images')
 
-    parser.add_argument('-o', '--output_dir', action='store', dest='output_dir', default='./output/' ,
+    parser.add_argument('-o', '--output_dir', action='store', dest='output_dir',
+                    # default='./output/' ,
+                    default='/media/ssd1/srgan_dataset/output',
                     help='Path for Output images')
 
-    parser.add_argument('-m', '--model_save_dir', action='store', dest='model_save_dir', default='./model/' ,
+    parser.add_argument('-m', '--model_save_dir', action='store', dest='model_save_dir',
+                    # default='./model/' ,
+                    default='/media/ssd1/srgan_dataset/model',
                     help='Path for model')
 
     parser.add_argument('-b', '--batch_size', action='store', dest='batch_size', default=64,
@@ -124,6 +147,9 @@ if __name__== "__main__":
 
     parser.add_argument('-r', '--train_test_ratio', action='store', dest='train_test_ratio', default=0.8 ,
                     help='Ratio of train and test Images', type=float)
+
+    parser.add_argument('-f', '--downscale_factor', action='store', dest='downscale_factor', default=4, # currently not used
+                    help='Downsampling scale factor', type= int)
 
     values = parser.parse_args()
 
